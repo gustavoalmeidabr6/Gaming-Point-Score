dd169aae4640d9540068940f7e623aa43d35f8ba
gustavoalmeidabr6 <emaildobr6@gmail.com>
2025-11-12 11:56:36 -0300

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
-    // O autoprefixer já está incluído no @tailwindcss/postcss,
-    // então não precisamos listá-lo duas vezes.
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
