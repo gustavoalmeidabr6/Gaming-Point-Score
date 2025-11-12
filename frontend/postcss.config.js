057e09f05712437be11abb12a565a2392f9263a1
gustavoalmeidabr6 <emaildobr6@gmail.com>
2025-11-12 12:14:57 -0300

    Delete postcss.config.js

--- frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    // AQUI ESTÁ A CORREÇÃO:
-    // Nós dizemos a ele para usar o pacote que instalamos
-    '@tailwindcss/postcss': {},
-    // 'autoprefixer' já está incluído no v4, então não precisamos dele aqui.
-  },
-}
