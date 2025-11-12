aa665a828f6b80d8be63ad2f48aaa00822dfa79b
gustavoalmeidabr6 <emaildobr6@gmail.com>
2025-11-12 11:50:43 -0300

    Update tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,8 +2,10 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}', // Diz ao Tailwind para olhar todas as nossas páginas
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}', // (Para o futuro)
+    // --- !!! AQUI ESTÁ A CORREÇÃO !!! ---
+    // Removemos o './src/' da frente dos caminhos.
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}', // (Para o futuro)
   ],
   theme: {
     extend: {},
