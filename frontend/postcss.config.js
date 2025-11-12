38179873758a4e6dde87073929455f4924d009c3
gustavoalmeidabr6 <emaildobr6@gmail.com>
2025-11-12 12:03:52 -0300

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
+    // AQUI ESTÁ A CORREÇÃO:
+    // Nós dizemos a ele para usar o pacote que instalamos
     '@tailwindcss/postcss': {},
-    // 'autoprefixer' já está incluído no '@tailwindcss/postcss' v4
+    // 'autoprefixer' já está incluído no v4, então não precisamos dele aqui.
   },
 }
